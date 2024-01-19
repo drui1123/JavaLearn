891aeeeb71bb50959f5643fa17df74b1d7eb8b50
drui <drui1123@163.com>
2024-01-19 17:35:49 +0800

    Lesson02：接着编写分支程序

--- src/HeimaLesson2/Structure/StructureSwitch.java
+++ src/HeimaLesson2/Structure/StructureSwitch.java
@@ -6,28 +6,28 @@ package HeimaLesson2.Structure;
 
 public class StructureSwitch {
     public static void main(String[] args) {
-        String day = "周三";
+        String day = "周二";
         switch (day){
             case "周一":
                 System.out.println("今天吃鸡");
                 break;
             case "周二":
-                System.out.println("");
+                System.out.println("今天刷怪");
                 break;
             case "周三":
-                System.out.println("");
+                System.out.println("今天撸铁");
                 break;
             case "周四":
-                System.out.println();
+                System.out.println("今天开会");
                 break;
             case "周五":
-                System.out.println();
+                System.out.println("今天回家");
                 break;
             case "周六":
-                System.out.println();
+                System.out.println("今天学习");
                 break;
             case "周日":
-                System.out.println();
+                System.out.println("今天加班");
                 break;
             default:
                 System.out.println("您输入的星期不正确");
