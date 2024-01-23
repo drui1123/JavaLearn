aaa4a5769b95a06661e952d8e1a408892ea8e7fe
drui <drui1123@163.com>
2024-01-23 16:45:04 +0800

    Lesson02：完成Switch分支结构学习

---
+++ src/HeimaLesson2/Structure/StructureFor.java
@@ -0,0 +1,10 @@
+package HeimaLesson2.Structure;
+/*
+掌握for的用法：
+*/
+
+public class StructureFor {
+    public static void main(String[] args) {
+
+    }
+}
--- src/HeimaLesson2/Structure/StructureSwitch.java
+++ src/HeimaLesson2/Structure/StructureSwitch.java
@@ -1,36 +1,37 @@
 package HeimaLesson2.Structure;
 
 /*
-掌握Switch的用法
+掌握Switch的用法：
+1.不支持double、float、long类型
+2.case给出的值不允许重复，且不能是变量
+3.缺少break时，具有穿透性
 */
 
 public class StructureSwitch {
     public static void main(String[] args) {
-        String day = "周二";
+        String day = "周日";
         switch (day){
             case "周一":
                 System.out.println("今天吃鸡");
                 break;
             case "周二":
-                System.out.println("今天刷怪");
-                break;
             case "周三":
-                System.out.println("今天撸铁");
-                break;
             case "周四":
-                System.out.println("今天开会");
+                System.out.println("今天开会"); // 不写break时，向下具有穿透性，顺序执行到第一个break
                 break;
             case "周五":
                 System.out.println("今天回家");
                 break;
             case "周六":
-                System.out.println("今天学习");
-                break;
             case "周日":
                 System.out.println("今天加班");
                 break;
             default:
                 System.out.println("您输入的星期不正确");
         }
+        // double类型数据的计算不精确
+        double b1=0.1;
+        double b2=0.2;
+        System.out.println(b1+b2);
     }
 }
